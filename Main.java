ad4182d80bfaf1ae3e3d687265fbc3588f148d42
Irina <70594426+irinazheltisheva@users.noreply.github.com>
2021-05-17 18:58:51 +0300

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,16 @@
+package ru.geekbrains.lesson1;
+
+class HomeWorkApp {
+
+    public static void main(String[] args) {
+    }
+    public static void printColor() {
+        int value;
+        value = 0;
+        System.out.println("Красный");
+        if (value > 100) {
+
+            System.out.println("Жёлтый");
+        }
+    }
+}
