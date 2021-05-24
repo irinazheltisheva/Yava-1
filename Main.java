45dc18ba356239bfba5ce28e84468831c0199fb5
Irina <70594426+irinazheltisheva@users.noreply.github.com>
2021-05-24 12:01:16 +0300

    Add files via upload

--- Main.java
+++ Main.java
@@ -1,13 +1,25 @@
-package ru.geekbrains.lesson1;
+package ru.geekbrains.lesson3;
 
 public class Main {
 
+    private static Object String;
+
     public static void main(String[] args) {
-	printThreeWorlds();
-    }
-    public static void printThreeWorlds() {
-    System.out.println("Orange");
-    System.out.println("Banana");
-    System.out.println("Apple");
-    }
+
+        (); {
+
+            int[] arr = {1, 1, 0, 0, 1, 0, 1, 1, 0, 0};
+            for (int i = 0; i < arr.length; i++) {
+                System.out.println(String[i] arr.length);
+
+            }
+
+        }
 }
+
+
+
+
+
+
+
